8ee763987e0a24c1e653796904ba7fe418692fde
Abel <abelwlk@foxmail.com>
2020-06-24 16:13:26 +0800

    讲师模块增删改查完成

--- service/service-edu/src/main/java/com/wlk/service/edu/controller/TeacherController.java
+++ service/service-edu/src/main/java/com/wlk/service/edu/controller/TeacherController.java
@@ -1,11 +1,17 @@
 package com.wlk.service.edu.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wlk.common.utils.R;
 import com.wlk.service.edu.entity.Teacher;
+import com.wlk.service.edu.entity.vo.TeacherQuery;
 import com.wlk.service.edu.service.TeacherService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +24,7 @@ import java.util.List;
  * @author wlk
  * @since 2020-06-24
  */
-@Api()
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/edu/teacher")
 public class TeacherController {
@@ -28,6 +34,7 @@ public class TeacherController {
     private TeacherService teacherService;
 
     //查询讲师所有数据
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("/findAll")
     public R findAllTeacher() {
         List<Teacher> list = teacherService.list(null);
@@ -35,12 +42,105 @@ public class TeacherController {
     }
 
     //根据id逻辑删除
+    @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("/{id}")
-    public R removeById(@PathVariable("id") String id) {
+    public R removeById(@ApiParam(name = "id", value = "讲师ID", required = true)
+                        @PathVariable("id") String id) {
         if (teacherService.removeById(id)) {
             return R.ok();
         } else
             return R.error();
     }
+
+    //分页查询讲师
+    @ApiOperation(value = "分页讲师列表")
+    @GetMapping("/pageTeacher/{current}/{limit}")
+    public R pageListTeacher(
+            @ApiParam(name = "current", value = "当前页码", required = true)
+            @PathVariable Long current,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit) {
+        //创建page对象
+        Page<Teacher> teacherPage = new Page<>(current, limit);
+        //调用方法实现分页
+        //方法底层自动封装
+        teacherService.page(teacherPage, null);
+
+        long total = teacherPage.getTotal();
+        List<Teacher> records = teacherPage.getRecords();
+
+        return R.ok().data("total", total).data("items", records);
+    }
+
+    //4 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current, @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<Teacher> pageTeacher = new Page<>(current, limit);
+
+        //构建条件
+        QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        //判断条件值是否为空，如果不为空拼接条件
+        if (!StringUtils.isEmpty(name)) {
+            //构建条件
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+
+        //排序
+        wrapper.orderByDesc("gmt_create");
+
+        //调用方法实现条件查询分页
+        teacherService.page(pageTeacher, wrapper);
+
+        long total = pageTeacher.getTotal();//总记录数
+        List<Teacher> records = pageTeacher.getRecords(); //数据list集合
+        return R.ok().data("total", total).data("rows", records);
+    }
+
+    //添加讲师
+    @ApiOperation(value = "新增讲师")
+    @PostMapping("/addTeacher")
+    public R save(@RequestBody Teacher teacher) {
+        boolean flag = teacherService.save(teacher);
+        if(flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+
+    @ApiOperation(value = "根据ID查询讲师")
+    @GetMapping("/{id}")
+    public R getById(@PathVariable String id) {
+
+        Teacher teacher = teacherService.getById(id);
+        return R.ok().data("item", teacher);
+    }
+
+    @ApiOperation(value = "根据ID修改讲师")
+    @PutMapping("/{id}")
+    public R updateById(@PathVariable String id,
+                        @RequestBody Teacher teacher) {
+
+        teacher.setId(id);
+        teacherService.updateById(teacher);
+        return R.ok();
+    }
+
 }
 
--- service/service-edu/src/main/java/com/wlk/service/edu/entity/Teacher.java
+++ service/service-edu/src/main/java/com/wlk/service/edu/entity/Teacher.java
@@ -1,11 +1,10 @@
 package com.wlk.service.edu.entity;
 
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -24,7 +23,7 @@ import lombok.experimental.Accessors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @TableName("edu_teacher")
-@ApiModel(value="Teacher对象", description="讲师")
+@ApiModel(value = "Teacher对象", description = "讲师")
 public class Teacher implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -56,9 +55,11 @@ public class Teacher implements Serializable {
     private Boolean isDeleted;
 
     @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
     private Date gmtCreate;
 
     @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date gmtModified;
 
 
